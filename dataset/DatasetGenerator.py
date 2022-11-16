bfcb3d72f68e6650625e006a7c558db9e22a0cf5
ante.pv5 <ap52554@fer.hr>
2022-11-16 19:14:09 +0100

    Generator for .csv file that links image inputs, gitignore added

---
+++ dataset/DatasetGenerator.py
@@ -0,0 +1,40 @@
+from os import listdir
+from os.path import isfile, join, abspath
+import random
+
+mypath = abspath(__file__).split('DatasetGenerator.py')[0]
+healthy_path = f'{mypath}Grape\\Grape___healthy'
+blackrot_path = f'{mypath}Grape\\Grape___Black_rot'
+esca_path = f'{mypath}Grape\\Grape___Esca_(Black_Measles)'
+blight_path = f'{mypath}Grape\\Grape___Leaf_blight_(Isariopsis_Leaf_Spot)'
+
+healthy = [f'{healthy_path}\\{f}' for f in listdir(healthy_path) if f.lower().endswith(('.jpg', '.png', 'jpeg'))]
+blackrot = [f'{blackrot_path}\\{f}' for f in listdir(blackrot_path) if f.lower().endswith(('.jpg', '.png', 'jpeg'))]
+esca = [f'{esca_path}\\{f}' for f in listdir(esca_path) if f.lower().endswith(('.jpg', '.png', 'jpeg'))]
+blight = [f'{blight_path}\\{f}' for f in listdir(blight_path) if f.lower().endswith(('.jpg', '.png', 'jpeg'))]
+
+healthy_dict = {key:'0001' for key in healthy}
+blackrot_dict = {key:'0010' for key in blackrot}
+esca_dict = {key:'0100' for key in esca}
+blight_dict = {key:'1000' for key in blight}
+
+total_data = dict()
+total_data.update(healthy_dict)
+total_data.update(blackrot_dict)
+total_data.update(esca_dict)
+total_data.update(blight_dict)
+
+temp = list(zip(total_data.keys(), total_data.values()))
+random.shuffle(temp)
+res1, res2 = zip(*temp)
+res1, res2 = list(res1), list(res2)
+ 
+import pandas as pd  
+      
+table = {'image': res1, 'output': res2}  
+       
+df = pd.DataFrame(table) 
+    
+# saving the dataframe 
+df.to_csv('in_out.csv') 
+
